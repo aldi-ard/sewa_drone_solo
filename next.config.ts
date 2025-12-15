68175398abf09d29a19e48836747a531eb7db8cd
aldi-ard <aldiardiansah657@gmail.com>
2025-12-15 07:56:03 +0700

    restructuring project

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
 
+module.exports = {
+  allowedDevOrigins: [
+    "http://localhost:3000",
+    "http://192.168.127.1:3000",
+  ],
+}
 export default nextConfig;
