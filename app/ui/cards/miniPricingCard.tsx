b3ff0edd387cf9ac5cdd61c02ded82f9723e97d4
aldi-ard <aldiardiansah657@gmail.com>
2025-12-19 21:14:13 +0700

    fixbug pricelist page

--- app/ui/cards/miniPricingCard.tsx
+++ app/ui/cards/miniPricingCard.tsx
@@ -12,6 +12,7 @@ export default function PricingCard({
   title,
   price,
   benefits,
+  jenis
 }: PricingCardProps) {
   return (
     <div className="w-full max-w-80 min-h-30 rounded-2xl border bg-white p-6 shadow-sm flex flex-col gap-4">
@@ -38,7 +39,7 @@ export default function PricingCard({
       <div className="mt-auto text-center">
         <Link
         className="h-full w-full bg-fuchsia-500 rounded-md"
-        href={`https://wa.me/6288802074999?text=Hai%20kak%2C%20aku%20mau%pesen%20nih%21%20Ini%20detailnya%3A%0A%0ANama%3A%20%5BNama%20Aku%5D%0APaket%3A%20${title}%0ATujuan%20%2F%20Lokasi%3A%20%5BLokasi%20pengambilan%20gambar%5D%0ATanggal%3A%20%5BTanggal%20kegiatan%5D%0A%0AMasih%20tersedia%20nggak%2C%20kak%3F%20Kalau%20ada%20rekomendasi%20lain%2C%20boleh%20banget%20ya.%20Thanks%20banget%20sebelumnya%20%F0%9F%98%8A`}
+        href={`https://wa.me/6288802074999?text=Hai%20kak%2C%20aku%20mau%pesen%20nih%21%20Ini%20detailnya%3A%0A%0ANama%3A%20%5BNama%20Aku%5D%0APaket%3A%20$${jenis}%20${title}%0ATujuan%20%2F%20Lokasi%3A%20%5BLokasi%20pengambilan%20gambar%5D%0ATanggal%3A%20%5BTanggal%20kegiatan%5D%0A%0AMasih%20tersedia%20nggak%2C%20kak%3F%20Kalau%20ada%20rekomendasi%20lain%2C%20boleh%20banget%20ya.%20Thanks%20banget%20sebelumnya%20%F0%9F%98%8A`}
         >
           <h1 className="capitalize font-semibold bg-fuchsia-500 py-2.5 rounded-md text-white">Kontak kami</h1>
         </Link>
